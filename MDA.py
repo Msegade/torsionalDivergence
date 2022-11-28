9b72cd0fa2a52689efe3938d5f298bcb3fcdcea3
Miguel Segade <miguelrsegade@gmail.com>
2022-11-28 17:40:22 +0100

    json missing

--- MDA.py
+++ MDA.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
+import json
 from Wing import Wing
 from time import time
 from config import octave, nastran
