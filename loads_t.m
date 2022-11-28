376deb0fd8cbe03fece60dabae459d6ac673b7e0
Miguel Segade <miguelrsegade@gmail.com>
2022-11-28 10:58:44 +0100

    Fixed element length

--- loads_t.m
+++ loads_t.m
@@ -24,7 +24,7 @@ function [load_Uy,load_Uz,load_Um] = loads_iteration(wing_Rx)
     %U_DT=850;
     U_DT={{U}};
     %U_DT = 1;
-    B=[3.6826652,3.385548667,3.08838,2.791584333,2.484609,2.187772667,1.890873,1.583790667,1.286951333,1]; 
+    B = [3.7, 3.4, 3.1, 2.8, 2.5, 2.2, 1.9, 1.6, 1.3, 1.0];
     nu_dyn_visc=1.5e-5;
     rho_air=1.22; % T
     Re_val=rho_air*U_DT*B/nu_dyn_visc;
@@ -37,10 +37,10 @@ function [load_Uy,load_Uz,load_Um] = loads_iteration(wing_Rx)
 
     %surf(angles_data, reynolds_data, Cd_data')
    
-    l_elemento = 0.9;
-    load_Uy=0.5* rho_air * (U_DT^2) * B       .*cd  * l_elemento;
-    load_Uz=0.5* rho_air * (U_DT^2) * B       .* cl * l_elemento;
-    load_Um=0.5* rho_air * (U_DT^2) * (B.^2)  .* cm * l_elemento;
+    l_elemento = [1.35, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.45];
+    load_Uy=0.5* rho_air * (U_DT^2) * B       .* cd .* l_elemento;
+    load_Uz=0.5* rho_air * (U_DT^2) * B       .* cl .* l_elemento;
+    load_Um=0.5* rho_air * (U_DT^2) * (B.^2)  .* cm .* l_elemento;
 
     save('load_Uy.mat', 'load_Uy')
     save('load_Uz.mat', 'load_Uz')
