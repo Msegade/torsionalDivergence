7088ec9c1978db1f8f105a31ea9c7f614e23cc0a
Miguel Segade <miguelrsegade@gmail.com>
2022-11-30 18:39:13 +0100

    Graphs in process results

--- processResults.py
+++ processResults.py
@@ -14,9 +14,9 @@ dirs = baseDir.glob('run*')
 destDir = baseDir / 'results'
 destDir.mkdir(exist_ok=True)
 
-U = np.linspace(50, 700, 14)
 
-udir = {}
+modesDir = {}
+Uconv = []
 for d in dirs:
     #ui = U[int(d.name.split('.')[1])-1]
     with (d / 'parameters.yaml').open('r') as f:
@@ -24,34 +24,42 @@ for d in dirs:
     ui = pDir['U']
     mod = d / 'model-modal-nLinear-restart.mod'
     xlsx = d / 'iteration-F-U.xlsx'
-    if mod.is_file():
-        copy(mod, destDir / f'{int(ui)}.mod')
-        copy(xlsx, destDir / f'{int(ui)}.xlsx')
+    # Check if converged
+    if not mod.is_file():
+        continue
+    Uconv.append(ui)
+    copy(mod, destDir / f'{int(ui)}.mod')
+    copy(xlsx, destDir / f'{int(ui)}.xlsx')
     file = d / 'results.json'
     if not file.is_file():
         print(f'{d} does not have results.json')
     else:
         with file.open('r') as f:
-            udir[file.parent.name]  = json.load(f)
+            modesDir[ui] = json.load(f)          
         
+#print(modesDir)
 
 #U = [50, 100, 150, 200, 250, 300, 350, 400, 450]
 #modesDir = {}
 #for i, ui in enumerate(U):
 #    modesDir[ui] = udir['run.' + str(i+1)]
 #
-#fig, ax = plt.subplots()
-#for i in range(1,11):
-#    mode = [modesDir[ui][f'mode-{i}'] for ui in U]
-#    #fig2 , ax2 = plt.subplots()
-#    #ax2.plot(U, mode, label=f'mode-{i}')
-#    #ax2.legend()
-#    #plt.savefig(f'mode-{i}.png', dpi=300)
-#    ax.plot(U, mode, label=f'mode-{i}')
-#
-##ax.legend()
-#plt.savefig(f'allModes.png', dpi=300)
-    
+
+U = np.sort(Uconv)
+print("U = ", U)
+fig, ax = plt.subplots()
+for ui in U:
+    for i in range(1,11):
+        mode = [modesDir[ui][f'mode-{i}'] for ui in U]
+        fig2 , ax2 = plt.subplots()
+        ax2.plot(U, mode, label=f'mode-{i}')
+        ax2.legend()
+        plt.savefig(destDir / f'mode-{i}.png', dpi=300)
+        plt.close(fig2)
+        ax.plot(U, mode, label=f'mode-{i}')
+
+#ax.legend()
+plt.savefig(destDir / f'allModes.png', dpi=300)
 
 
 
